572f4995352b5b2ac652327abef80b0510ed458e
Scott Shrout <qscott86@gmail.com>
2025-04-24 20:18:08 -0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -31828,16 +31828,12 @@ module.exports = parseParams
 var __webpack_exports__ = {};
 const core = __nccwpck_require__(6552)
 const github = __nccwpck_require__(7608)
+const fs = __nccwpck_require__(9896)
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    fs.readdir("/", (error, files) => {
+        console.log(files);
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
