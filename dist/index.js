3719f39dfad0e95e217a8c9aeb1327e43f1688c3
Scott Shrout <qscott86@gmail.com>
2025-04-24 20:12:30 -0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -15,14 +15,6 @@ module.exports = eval("require")("@actions/core");
 module.exports = eval("require")("@actions/github");
 
 
-/***/ }),
-
-/***/ 896:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("fs");
-
 /***/ })
 
 /******/ 	});
@@ -66,12 +58,16 @@ module.exports = require("fs");
 var __webpack_exports__ = {};
 const core = __nccwpck_require__(396)
 const github = __nccwpck_require__(444)
-const fs = __nccwpck_require__(896)
 
 try {
-    fs.readdir("/", (error, files) => {
-        console.log(files);
-    })
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
