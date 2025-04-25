350157a8de9b82b3879da541ef0f26aefae7178a
Scott Shrout <qscott86@gmail.com>
2025-04-24 20:28:36 -0400

    Test && git push

--- dist/index.js
+++ dist/index.js
@@ -31831,9 +31831,7 @@ const github = __nccwpck_require__(7608)
 const fs = __nccwpck_require__(9896)
 
 try {
-    fs.readdir("/", (error, files) => {
-        console.log(files);
-    })
+   console.log(process.env);
   } catch (error) {
     core.setFailed(error.message);
   }
